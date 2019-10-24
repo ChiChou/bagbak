d63d91834135416e1f0dcff90dbdbf89c113966a
CodeColorist <codecolorist@gmail.com>
2019-10-24 19:08:13 +0800

    add a new approach to list plugins

--- agent/pluginkit.ts
+++ agent/pluginkit.ts
@@ -9,3 +9,26 @@ export function skipPkdValidationFor(pid: number) {
     })
   }
 }
+
+export function plugins() {
+  const {
+    LSApplicationWorkspace,
+    NSString,
+    NSMutableArray,
+    NSPredicate,
+    NSBundle
+  } = ObjC.classes;
+
+  const args = NSMutableArray.alloc().init();
+  args.setObject_atIndex_(NSBundle.mainBundle().bundleIdentifier(), 0);
+  const fmt = NSString.stringWithString_('containingBundle.applicationIdentifier=%@');
+  const predicate = NSPredicate.predicateWithFormat_argumentArray_(fmt, args);
+  const plugins = LSApplicationWorkspace.defaultWorkspace()
+    .installedPlugins().filteredArrayUsingPredicate_(predicate);
+  const result = [];
+  for (let i = 0; i < plugins.count(); i++) {
+    result.push(plugins.objectAtIndex_(i).pluginIdentifier().toString());
+  }
+  args.release();
+  return result;
+}
