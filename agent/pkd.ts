71999efe42865c88f3580c09352ac04a4bbc9c9a
CodeColorist <ChiChou@users.noreply.github.com>
2021-01-26 15:36:39 +0000

    fix #59 new method introduced by iOS14

--- agent/pkd.ts
+++ agent/pkd.ts
@@ -7,12 +7,19 @@ export function jetsam(pid: number) {
 
 export function skipPkdValidationFor(pid: number) {
   if ('PKDPlugIn' in ObjC.classes) {
-    const method = ObjC.classes.PKDPlugIn['- allowForClient:'];
-    const original = method.implementation;
-    method.implementation = ObjC.implement(method, function(self, sel, conn) {
-      // race condition huh? we don't care
-      return pid === new ObjC.Object(conn).pid() ?
-        NULL : original(self, sel, conn);
-    })
+    const { PKDPlugIn } = ObjC.classes;
+    const canidates = ['- allowForClient:discoveryInstanceUUID:', '- allowForClient:'];
+    for (const name of canidates) {
+      const method = PKDPlugIn[name];
+      if (method) {
+        const original = method.implementation;
+        method.implementation = ObjC.implement(method, function(self, sel, conn) {
+          // race condition huh? we don't care
+          return pid === new ObjC.Object(conn).pid() ?
+            NULL : original(self, sel, conn);
+        })
+        break;
+      }
+    }
   }
 }
